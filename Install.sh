00e8a55c0016a340c09641b4996558d9895c38e9
Matt Webb <matt531320.efreak@gmail.com>
2018-08-21 18:46:45 -0700

    Almost there

--- Install.sh
+++ Install.sh
@@ -17,16 +17,18 @@ done
 HOMEDIR=~
 
 echo "export DISPLAY=:0" > ~/rpi-webpage-display-atBoot.sh
-echo "chromium-browser --no-first-run --disable --disable-translate --disable-infobars ----disable-session-crashed-bubble --disable-suggestions-service --disable-save-password-bubble --start-maximized --kiosk \"${pageURL}\" &" >> ~/rpi-webpage-display-atBoot.sh
-echo "xset s off && xset -dpms && xset s noblank" >> ~/rpi-webpage-display-atBoot.sh
+echo "rm -rf ~/.cache/chromium/*" >> ~/rpi-webpage-display-atBoot.sh
+echo "chromium-browser --no-first-run --disable --disable-translate --disable-infobars ----disable-session-crashed-bubble --disable-translate --disable-suggestions-service --disable-save-password-bubble --noerrdialogs --start-maximized --kiosk \"${pageURL}\" &" >> ~/rpi-webpage-display-atBoot.sh
 sudo chmod +x "${HOMEDIR}/rpi-webpage-display-atBoot.sh"
 (crontab -l 2>/dev/null; echo "@reboot ${HOMEDIR}/rpi-webpage-display-atBoot.sh") | crontab -
 
 if [[ "$hideCursor" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     sudo apt-get install unclutter -y
-    echo "unclutter -idle 0.01 -root" >> ~/rpi-webpage-display-atBoot.sh
+    echo "unclutter -idle 0.01 -root &" >> ~/rpi-webpage-display-atBoot.sh
 fi
+
+echo "xset s off && xset -dpms && xset s noblank" >> ~/rpi-webpage-display-atBoot.sh
 
 if [ $refreshRate -gt 0 ]
 then
@@ -35,6 +37,9 @@ then
     echo "xdotool search --onlyvisible --class \"Chromium\" windowfocus key 'ctrl+r'" >> ~/rpi-webpage-display-Refresh.sh
     sudo chmod +x "${HOMEDIR}/rpi-webpage-display-Refresh.sh"
     (crontab -l 2>/dev/null; echo "*/${refreshRate} * * * * ${HOMEDIR}/rpi-webpage-display-Refresh.sh") | crontab -
+
+    # Refresh page after boot in case networking wasn't fully up or other issue
+    echo "sleep 120 && xdotool search --onlyvisible --class \"Chromium\" windowfocus key 'ctrl+r'" >> ~/rpi-webpage-display-atBoot.sh
 fi
 
 echo "Done! Rebooting now..."
