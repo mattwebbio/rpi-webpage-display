832875013b86186ed762cfd9831cd74a1fa915a0
Matt Webb <matt531320.efreak@gmail.com>
2018-08-21 18:32:56 -0700

    Almost everything I need

--- Install.sh
+++ Install.sh
@@ -5,6 +5,8 @@ read pageURL
 printf -v pageURL "%q\n" "$pageURL"
 echo "How often (in minutes) do you want the page to refresh? (0 to disable)"
 read refreshRate
+read -r -p "Hide cursor? (Good for non-interactive displays) [y/N] " hideCursor
+
 while ! [[ $refreshRate =~ ^-?[0-9]+$ ]]
 do
     echo "Error: please enter an integer."
@@ -16,8 +18,15 @@ HOMEDIR=~
 
 echo "export DISPLAY=:0" > ~/rpi-webpage-display-atBoot.sh
 echo "chromium-browser --no-first-run --disable --disable-translate --disable-infobars ----disable-session-crashed-bubble --disable-suggestions-service --disable-save-password-bubble --start-maximized --kiosk \"${pageURL}\" &" >> ~/rpi-webpage-display-atBoot.sh
+echo "xset s off && xset -dpms && xset s noblank"
 sudo chmod +x "${HOMEDIR}/rpi-webpage-display-atBoot.sh"
 (crontab -l 2>/dev/null; echo "@reboot ${HOMEDIR}/rpi-webpage-display-atBoot.sh") | crontab -
+
+if [[ "$hideCursor" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    sudo apt-get install unclutter -y
+    echo "unclutter -idle 0.01 -root" >> ~/rpi-webpage-display-atBoot.sh
+fi
 
 if [ $refreshRate -gt 0 ]
 then
